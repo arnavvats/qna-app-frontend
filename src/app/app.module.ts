f98c6e45df35860e11f8c8071160e3d28779d0b4
Arnav Vats <arnavvats97@gmail.com>
2019-05-09 04:05:18 +0530

    Renamed questions to question list to avoid confusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopicsComponent } from './pages/topics/topics.component';
-import { QuestionsComponent } from './pages/questions/questions.component';
 import { QuestionPreviewComponent } from './shared/components/question-preview/question-preview.component';
 import { TopicPreviewComponent } from './shared/components/topic-preview/topic-preview.component';
 import { QuestionComponent } from './pages/question/question.component';
+import { QuestionListComponent } from './pages/question-list/question-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopicsComponent,
-    QuestionsComponent,
     QuestionPreviewComponent,
     TopicPreviewComponent,
-    QuestionComponent
+    QuestionComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule,
