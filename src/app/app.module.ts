c26350e3a9aa3008b615dde204c6f565cdaf6aff
Arnav Vats <arnavvats97@gmail.com>
2019-05-09 03:56:09 +0530

    Added skeletal setup and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopicsComponent } from './pages/topics/topics.component';
+import { QuestionsComponent } from './pages/questions/questions.component';
+import { QuestionPreviewComponent } from './shared/components/question-preview/question-preview.component';
+import { TopicPreviewComponent } from './shared/components/topic-preview/topic-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopicsComponent,
+    QuestionsComponent,
+    QuestionPreviewComponent,
+    TopicPreviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/topic-preview/topic-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-preview',
+  templateUrl: './topic-preview.component.html',
+  styleUrls: ['./topic-preview.component.scss']
+})
+export class TopicPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
