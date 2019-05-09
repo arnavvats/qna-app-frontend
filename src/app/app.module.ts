b04e68fc654094647f3edb44b0a10f70a9e2f78a
Arnav Vats <arnavvats97@gmail.com>
2019-05-09 16:24:14 +0530

    App module listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { QuestionPreviewComponent } from './shared/components/question-preview/q
 import { TopicPreviewComponent } from './shared/components/topic-preview/topic-preview.component';
 import { QuestionComponent } from './pages/question/question.component';
 import { QuestionListComponent } from './pages/question-list/question-list.component';
+import { NavbarComponent } from './ui/navbar/navbar.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {CustomHttpInterceptor} from './shared/services/interceptor.service';
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,13 +22,21 @@ import { QuestionListComponent } from './pages/question-list/question-list.compo
     QuestionPreviewComponent,
     TopicPreviewComponent,
     QuestionComponent,
-    QuestionListComponent
+    QuestionListComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: CustomHttpInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
