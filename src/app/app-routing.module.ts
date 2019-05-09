0d19f9632a33f811c38da7df31e832968ed735ba
Arnav Vats <arnavvats97@gmail.com>
2019-05-09 16:24:39 +0530

    Routes ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,32 @@ import { Routes, RouterModule } from '@angular/router';
 import {TopicsComponent} from './pages/topics/topics.component';
 import {QuestionComponent} from './pages/question/question.component';
 import {QuestionListComponent} from './pages/question-list/question-list.component';
+import {LoginComponent} from './pages/login/login.component';
+import {UnAuthGuard} from './shared/guards/un-auth.guard';
+import {QuestionListService} from './pages/question-list/question-list.service';
+import {TopicsService} from './pages/topics/topics.service';
+import {QuestionService} from './pages/question/question.service';
 
 const routes: Routes = [
   {
     path: 'topics',
-    component: TopicsComponent
+    component: TopicsComponent,
+    resolve: {resolved: TopicsService}
   },
   {
     path: 'questions',
-    component: QuestionListComponent
+    component: QuestionListComponent,
+    resolve: { resolved: QuestionListService}
   },
   {
     path: 'question/:id',
-    component: QuestionComponent
+    component: QuestionComponent,
+    resolve: { resolved:  QuestionService}
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [UnAuthGuard]
   },
   {
     path: '**',
